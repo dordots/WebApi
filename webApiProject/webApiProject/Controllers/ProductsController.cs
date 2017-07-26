cdcd70fa1e2942b902bf5397cf46e68e4d90a612
Dor Saban <dordots@gmail.com>
2017-07-26 11:21:15 +0300

    add test configuration

--- webApiProject/webApiProject/Controllers/ProductsController.cs
+++ webApiProject/webApiProject/Controllers/ProductsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
 using webApiProject.Models;
 
@@ -10,41 +11,75 @@ namespace webApiProject.Controllers
 {
     public class ProductsController : ApiController
     {
-        Product[] products = new Product[] {
+        List<Product> products = new List<Product>();
 
-            new Product {Id = 1,Name = "a",Category = "i", Price = 1 },
-            new Product {Id = 2,Name = "b",Category = "ii", Price = 2},
-            new Product {Id = 3,Name = "c",Category = "iii", Price = 3},
-            new Product {Id = 4,Name = "d",Category = "iiii", Price = 4}
-        };
-        // GET api/<controller>
+        public ProductsController() { }
+
+        public ProductsController(List<Product> products)
+        {
+            this.products = products;
+        }
+
         public IEnumerable<Product> GetAllProducts()
         {
             return products;
         }
 
-        public IHttpActionResult GetProduct(int id) {
-            var product = products.FirstOrDefault((p) => p.Id == id);
+        public async Task<IEnumerable<Product>> GetAllProductsAsync()
+        {
+            return await Task.FromResult(GetAllProducts());
+        }
 
-            if (product == null) {
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = products.FirstOrDefault((p) => p.Id == id);
+            if (product == null)
+            {
                 return NotFound();
             }
-            return Ok(product + "hello" );
+            return Ok(product);
         }
 
-        // POST api/<controller>
-        public void Post([FromBody]string value)
+        public async Task<IHttpActionResult> GetProductAsync(int id)
         {
+            return await Task.FromResult(GetProduct(id));
         }
 
-        // PUT api/<controller>/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
+        //Product[] products = new Product[] {
 
-        // DELETE api/<controller>/5
-        public void Delete(int id)
-        {
-        }
+        //    new Product {Id = 1,Name = "a",Category = "i", Price = 1 },
+        //    new Product {Id = 2,Name = "b",Category = "ii", Price = 2},
+        //    new Product {Id = 3,Name = "c",Category = "iii", Price = 3},
+        //    new Product {Id = 4,Name = "d",Category = "iiii", Price = 4}
+        //};
+        //// GET api/<controller>
+        //public IEnumerable<Product> GetAllProducts()
+        //{
+        //    return products;
+        //}
+
+        //public IHttpActionResult GetProduct(int id) {
+        //    var product = products.FirstOrDefault((p) => p.Id == id);
+
+        //    if (product == null) {
+        //        return NotFound();
+        //    }
+        //    return Ok(product + "hello" );
+        //}
+
+        //// POST api/<controller>
+        //public void Post()
+        //{
+        //}
+
+        //// PUT api/<controller>/5
+        //public void Put(int id, [FromBody]string value)
+        //{
+        //}
+
+        //// DELETE api/<controller>/5
+        //public void Delete(int id)
+        //{
+        //}
     }
 }
--- webApiProject/webApiProject/Models/Product.cs
+++ webApiProject/webApiProject/Models/Product.cs
@@ -9,7 +9,6 @@ namespace webApiProject.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Category { get; set; }
         public decimal Price { get; set; }
     }
 }
