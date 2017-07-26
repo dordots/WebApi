7cf373f62362d559527e52e1048810261a507916
Dor Saban <dordots@gmail.com>
2017-07-27 00:13:37 +0300

    add ambition model , enum and start configure mongoDb

---
+++ webApiProject/webApiProject/Controllers/AmbitionController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using webApiProject.Models;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace webApiProject.Controllers
+{
+    public class AmbitionController : ApiController
+    {
+        List<Ambition> ambitions = new List<Ambition>();
+
+        public AmbitionController() { }
+
+        public AmbitionController(List<Ambition> ambitions)
+        {
+            this.ambitions = ambitions;
+        }
+
+        public IEnumerable<Ambition> GetAllProducts()
+        {
+            return ambitions;
+        }
+
+        public async Task<IEnumerable<Ambition>> GetAllProductsAsync()
+        {
+            return await Task.FromResult(GetAllProducts());
+        }
+    }
+}
---
+++ webApiProject/webApiProject/Models/Ambition.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using webApiProject.Enums;
+
+namespace webApiProject.Models
+{
+    public class Ambition
+    {
+        public string Title { get; set; }
+        public string Note { get; set; }
+        public bool Recurrence { get; set; }
+        public string Partner { get; set; }
+        public AmbitionTypeEnum AmbitionType { get; set; }
+    }
+}
